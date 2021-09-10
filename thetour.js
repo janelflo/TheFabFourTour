d97484143ddfb811d9ffdc8c1f2d4ac4cfb21cd4
richdavidowski <74685395+richdavidowski@users.noreply.github.com>
2021-09-09 23:23:11 -0600

    Created the render function for tour dates to display in table

--- thetour.js
+++ thetour.js
@@ -1,5 +1,5 @@
 //Global Variables
-let id = 0; //Assign an ID to update and delete
+let id = 1; //Assign an ID to update and delete
 let tourDates = []; //store the entities we create, read, update, and delete
 
 //Data Structures -- pushing this class to our table
@@ -8,6 +8,7 @@ class TourDate {
 		this.date = date;
 		this.city = city;
 		this.location = location;
+
 	}
 
 	createTourDate() {
@@ -29,10 +30,57 @@ class DOMManager {
 			let tourDate = new TourDate(date, city, location);
 			tourDate.createTourDate();
 			console.log(tourDates);
+			document.getElementById('date').value = '';
+			document.getElementById('city').value = '';
+			document.getElementById('location').value = '';
+
+			return dom.renderTourDates();
 			//When we click, we want to get the value from each input and pass tht input into our
 			//tourDate class and then call upon on createTourDate method
 			//Grab each value from each input and pass as an instance of the TourDate Class
 		});
+	}
+
+	getAllTourDates() {
+		document.getElementsByClassName('showInfo');
+	}
+
+
+	// renderTourDates() {
+	// 	this.tourDates = tourDates;
+	// 	$('#test').empty();
+	// 	for (let tourDate of tourDates) {
+	// 		$('#test').prepend(
+	// 			`
+	// 			<div class="card">
+	// 			<div class="card-header">
+	// 				<h3>${tourDate.date}</h3>
+	// 			</div>
+	// 			<div class="card-body">
+
+	// 			</div>
+	// 			`
+	// 		)
+	// 	}
+	// }
+
+
+	renderTourDates() {
+		this.tourDates = tourDates;
+		// console.log(tourDates);
+		$('.test').empty();
+		for (let tourDate of tourDates) {
+			$('#tour-table').prepend(
+				`<tr class="text-center test">
+				<td><button class="btn btn-primary" onclick="dom.editButton()">Edit</button></td>
+				<td>${tourDate.date}</td>
+				<td>${tourDate.city}</td>
+				<td>${tourDate.location}</td>
+				<td><button class="btn btn-danger" onclick="dom.deleteButton()">Delete</button></td>
+				</tr>
+				`
+			);
+		}
 	}
 
 	//getAllTourDates -- get all info anytime we make changes to the DOM -- Display --Richie
@@ -42,4 +90,5 @@ class DOMManager {
 }
 let dom = new DOMManager();
 dom.addButton();
+dom.renderTourDates();
 //
